e8fe0287a25abe68246c485d0766ebf244be44b3
hcmaza <hcmaza@gmail.com>
2019-11-14 23:07:23 -0300

    examen 2 seminario alumnos

---
+++ src/app/detalle-libro/detalle-libro.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Libro } from '../model/Libro';
+import { ActivatedRoute } from '@angular/router';
+import { LibroService } from '../services/libro.service';
+
+@Component({
+  selector: 'app-detalle-libro',
+  templateUrl: './detalle-libro.page.html',
+  styleUrls: ['./detalle-libro.page.scss'],
+})
+export class DetalleLibroPage implements OnInit {
+  
+  libro: Libro = {id: 0, titulo: '', cantidadpaginas: 0, fechapublicacion: new Date()} ;
+  constructor(private route: ActivatedRoute, private ps: LibroService) { }
+  
+  ngOnInit() {
+      this.getLibro();
+  }
+
+  getLibro(): void {
+    if ( this.route.snapshot.paramMap.get('id') !== '0') {
+      const id = +this.route.snapshot.paramMap.get('id');
+      
+    }
+
+  }
+  addPersona() {
+    if (this.libro.id===0){
+      this.ps.postLibro(this.libro).subscribe(
+        data => {
+          console.log(data);
+        }
+      );
+    } else {
+      this.ps.putLibro(this.libro).subscribe(
+        data => {
+          console.log(data);
+        }
+      );
+    }
+    
+  }
+
+}
